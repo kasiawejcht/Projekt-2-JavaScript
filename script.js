288e7e8ef494de4891032392a89129f66c48eec7
kasiawejcht <110462029+kasiawejcht@users.noreply.github.com>
2023-03-19 15:14:34 +0100

    Add files via upload
    2 javasript

---
+++ script.js
@@ -0,0 +1,33 @@
+const loader = document.querySelector(".loader");
+const selectCurrency = document.getElementById("currency");
+const amountInput = document.getElementById("amount");
+const convertButton = document.getElementById("convert");
+const resultParagraph = document.getElementById("result");
+
+convertButton.addEventListener("click", convertCurrency);
+
+function convertCurrency() {
+  const currencyCode = selectCurrency.value;
+  const amount = amountInput.value;
+  if (amount === "") {
+    alert("Proszę wpisać kwotę");
+    return;
+  }
+  //http://api.nbp.pl/api/exchangerates/rates/{table}/{code}/
+  loader.style.display = "block";
+  fetch(`https://api.nbp.pl/api/exchangerates/rates/a/${currencyCode}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      const rate = data.rates[0].mid;
+      const convertedAmount = amount * rate;
+      resultParagraph.innerHTML = `${amount} ${currencyCode} = ${convertedAmount.toFixed(
+        2
+      )} PLN`;
+      loader.style.display = "none";
+    })
+    .catch((error) => {
+      alert("Wystąpił błąd. Spróbuj ponownie później.");
+      console.error(error);
+      loader.style.display = "none";
+    });
+}
